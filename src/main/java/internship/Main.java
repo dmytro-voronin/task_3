4fb2fa4645d7b4bd95bf8c343bb35ffb267e7426
dmytro-voronin <d.voronin.owen@gmail.com>
2024-05-30 17:02:26 +0300

    fix comment

--- src/main/java/internship/Main.java
+++ src/main/java/internship/Main.java
@@ -2,7 +2,6 @@ package internship;
 
 public class Main {
     public static void main(String[] args) {
-        // Пример использования
         String message = "GO";
         String encryptedMessage = MessageEncryptor.encryptMessage(message);
         System.out.println("Encrypted message: " + encryptedMessage);
